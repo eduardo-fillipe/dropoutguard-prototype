6729fbe1c0cbc75b308fad08cc71b7dff873b01e
Eduardo Reis <eduardo556@live.com>
2024-12-08 11:27:16 -0300

    initial configuration

---
+++ src/main/java/br/ufs/dcomp/dropoutguard/DropoutguardApplication.java
@@ -0,0 +1,13 @@
+package br.ufs.dcomp.dropoutguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropoutguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DropoutguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufs/dcomp/dropoutguard/TestDropoutguardApplication.java
@@ -0,0 +1,10 @@
+package br.ufs.dcomp.dropoutguard;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestDropoutguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(DropoutguardApplication::main).run(args);
+	}
+}
